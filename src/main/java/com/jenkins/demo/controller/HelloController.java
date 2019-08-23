1b8aa3709958fb1e85acedb6f42ef26cb2a47341
wanjun <13476515972@163.com>
2019-08-23 13:50:50 +0800

    jenkins测试

---
+++ src/main/java/com/jenkins/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package com.jenkins.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloController {
+   @RequestMapping(value = "/index",method = RequestMethod.GET)
+   public String hellohtml ()
+   {
+       return "/index.html";
+   }
+}
